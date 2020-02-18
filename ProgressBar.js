d144713e02e02cb7e0ff93af9b1b399469ef80ca
MΛX <34509813+Mw3y@users.noreply.github.com>
2020-02-18 16:15:35 +0100

    Progressbar

--- ProgressBar.js
+++ ProgressBar.js
@@ -1,25 +1,3 @@
-const Tools = require('../utils/Tools');
-const ServerManager = require('../classes/ServerManager');
-let Text;
-
-/**
- * Allow to charge the correct text file
- * @param message - The message that caused the function to be called. Used to retrieve the author of the message.
- */
-const chargeText = async function (message) {
-    let serverManager = new ServerManager();
-    let server = await serverManager.getServer(message);
-    if (message.channel.id == 639446722845868101) {
-        server.language = "en";
-    }
-    let address = '../text/' + server.language;
-    return require(address);
-}
-
-/**
- * Base class that shouldn't be instantiated. Instead, Entities are meant to extend this class.
- * Entities are things like Enemies, entitys...
- */
 class ProgressBar {
     constructor(value, maxValue, barSize) {
             this.value = value;
