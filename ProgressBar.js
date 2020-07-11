8b335069aef89bc87f3ad514c712fd34d771fd99
MΛX <34509813+Mw3y@users.noreply.github.com>
2020-07-11 11:28:23 +0200

    Update ProgressBar.js

--- ProgressBar.js
+++ ProgressBar.js
@@ -1,27 +1,19 @@
-class ProgressBar {
-    constructor(value, maxValue, barSize) {
-            this.value = value;
-            this.maxValue = maxValue;
-            this.barSize = barSize;
-    }
+/**
+ * Create a text progress bar
+ * @param {Number} value - The bar percentage
+ * @param {Number} maxValue - The max percentage of the bar
+ * @param {Number} size - The bar size (in letters)
+ * @return {String} - The bar
+ */
+global.progressBar = (value, maxValue, size) => {
+  const percentage = value / maxValue; // Calculate the percentage of the bar
+  const progress = Math.round((size * percentage)); // Calculate the number of square caracters to fill the progress side.
+  const emptyProgress = size - progress; // Calculate the number of dash caracters to fill the empty progress side.
 
-    /**
-     * Create a text progress bar
-     * @returns {String} - The bar
-     */
-    createBar() {
-        let percentage = this.value / this.maxValue; //Calculate the percentage of the bar
-        let progress = Math.round((this.barSize * percentage)); //Calculate the number of square caracters to fill the progress side.
-        let emptyProgress = this.barSize - progress; //Calculate the number of dash caracters to fill the empty progress side.
+  const progressText = '▇'.repeat(progress); // Repeat is creating a string with progress * caracters in it
+  const emptyProgressText = '—'.repeat(emptyProgress); // Repeat is creating a string with empty progress * caracters in it
+  const percentageText = Math.round(percentage * 100) + '%'; // Displaying the percentage of the bar
 
-        let progressText = "▇".repeat(progress); //Repeat is creating a string with progress * caracters in it
-        let emptyProgressText = "—".repeat(emptyProgress); //Repeat is creating a string with empty progress * caracters in it
-        let percentageText = Math.round(percentage * 100) + "%"; //Displaying the percentage of the bar
-
-        let bar = "[" + progressText + emptyProgressText + "] " + percentageText; //Creating the bar
-        return bar;
-    }
-
-}
-
-module.exports = ProgressBar;
+  const bar = '```[' + progressText + emptyProgressText + ']' + percentageText + '```'; // Creating the bar
+  return bar;
+};
